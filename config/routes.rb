75471a7e8677f574e59ac63724355945934eb636
Kelvin <vokemalosh32@gmail.com>
2022-09-30 13:40:19 +0300

    commit tree 50c479c651a2c6810646e631e89feaf47310010d

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
-end
+  # resources :items, only: [:index]
+  # resources :users, only: [:show]
+
+  resources :users, only: [:show] do
+    # nested resource for items
+    resources :items, only: [:show, :index, :create]
+  end
+
+  resources :items, only: [:show, :index]
+end 
+ 
